3f697cb30e67a1eda1a609b120ed28a48dc0fbd5
codinggeet <89382233+codinggeet@users.noreply.github.com>
2021-10-02 13:55:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var block;
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{ 
+  background(30);
+  if(keyIsDown(DOWN_ARROW))
+  {
+    background('red')
+  }
+  if(keyIsDown(RIGHT_ARROW))
+  {
+    background('blue')
+  }
+  if(keyIsDown(LEFT_ARROW))
+  {
+    background('pink')
+  }
+  if(keyIsDown(UP_ARROW))
+  {
+    background('yellow')
+  }
+
+}
+
+drawSprites();
+
+
